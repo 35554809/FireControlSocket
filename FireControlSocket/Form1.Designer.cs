f83df63a0a0badc75d90b12a2693a650b2cd5689
35554809 <35554809@qq.com>
2017-05-24 14:31:59 +0800

    All

---
+++ FireControlSocket/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace FireControlSocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.db_Address = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.db_Name = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.db_Pass = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.db_BaseName = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.db_ColumnInfo = new System.Windows.Forms.TextBox();
+            this.db_ColumnTime = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.db_TableName = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "端口号：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "开始接收";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(20, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "发送内容：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(91, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "TCP发送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(108, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "UDP发送";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "接收方式：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(91, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.db_TableName);
+            this.groupBox1.Controls.Add(this.db_BaseName);
+            this.groupBox1.Controls.Add(this.db_ColumnTime);
+            this.groupBox1.Controls.Add(this.db_Pass);
+            this.groupBox1.Controls.Add(this.db_ColumnInfo);
+            this.groupBox1.Controls.Add(this.db_Name);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.db_Address);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(213, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 163);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "数据库信息配置";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "数据库地址：";
+            // 
+            // db_Address
+            // 
+            this.db_Address.Location = new System.Drawing.Point(90, 18);
+            this.db_Address.Name = "db_Address";
+            this.db_Address.Size = new System.Drawing.Size(195, 21);
+            this.db_Address.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "登录名：";
+            // 
+            // db_Name
+            // 
+            this.db_Name.Location = new System.Drawing.Point(66, 47);
+            this.db_Name.Name = "db_Name";
+            this.db_Name.Size = new System.Drawing.Size(64, 21);
+            this.db_Name.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(136, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "密码：";
+            // 
+            // db_Pass
+            // 
+            this.db_Pass.Location = new System.Drawing.Point(173, 47);
+            this.db_Pass.Name = "db_Pass";
+            this.db_Pass.Size = new System.Drawing.Size(112, 21);
+            this.db_Pass.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 12);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "数据库名：";
+            // 
+            // db_BaseName
+            // 
+            this.db_BaseName.Location = new System.Drawing.Point(66, 74);
+            this.db_BaseName.Name = "db_BaseName";
+            this.db_BaseName.Size = new System.Drawing.Size(64, 21);
+            this.db_BaseName.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 107);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 12);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "信息字段：";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(136, 107);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 12);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "时间字段：";
+            // 
+            // db_ColumnInfo
+            // 
+            this.db_ColumnInfo.Location = new System.Drawing.Point(66, 104);
+            this.db_ColumnInfo.Name = "db_ColumnInfo";
+            this.db_ColumnInfo.Size = new System.Drawing.Size(64, 21);
+            this.db_ColumnInfo.TabIndex = 6;
+            // 
+            // db_ColumnTime
+            // 
+            this.db_ColumnTime.Location = new System.Drawing.Point(195, 104);
+            this.db_ColumnTime.Name = "db_ColumnTime";
+            this.db_ColumnTime.Size = new System.Drawing.Size(90, 21);
+            this.db_ColumnTime.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 130);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "确认保存";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(136, 78);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 12);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "数据表名：";
+            // 
+            // db_TableName
+            // 
+            this.db_TableName.Location = new System.Drawing.Point(195, 74);
+            this.db_TableName.Name = "db_TableName";
+            this.db_TableName.Size = new System.Drawing.Size(85, 21);
+            this.db_TableName.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 188);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "数据接收";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox db_Address;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox db_Name;
+        private System.Windows.Forms.TextBox db_Pass;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox db_BaseName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox db_ColumnTime;
+        private System.Windows.Forms.TextBox db_ColumnInfo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox db_TableName;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ FireControlSocket/Form1.cs
@@ -0,0 +1,239 @@
+﻿using FireControlSocket.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using FireControlSocket.DB;
+
+namespace FireControlSocket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static string TableName;
+        private static string ColumnInfo;
+        private static string ColumnTime;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("UDP");
+            comboBox1.Items.Add("TCP Client");
+            comboBox1.Items.Add("TCP Server");
+            comboBox1.SelectedIndex = 2;
+
+            Ini.strFilePath = Application.StartupPath + "\\Config.ini";
+            var strSec = Path.GetFileNameWithoutExtension(Ini.strFilePath);
+            db_Address.Text = Ini.ContentValue(strSec, "db_Address");
+            db_Name.Text = Ini.ContentValue(strSec, "db_Name");
+            db_Pass.Text = Ini.ContentValue(strSec, "db_Pass");
+            db_BaseName.Text = Ini.ContentValue(strSec, "db_BaseName");
+            db_TableName.Text = Ini.ContentValue(strSec, "db_TableName");
+            db_ColumnInfo.Text = Ini.ContentValue(strSec, "db_ColumnInfo");
+            db_ColumnTime.Text = Ini.ContentValue(strSec, "db_ColumnTime");
+
+            TableName = db_TableName.Text;
+            ColumnInfo = db_ColumnInfo.Text;
+            ColumnTime = db_ColumnTime.Text;
+
+            DbHelperSQL.connectionString = string.Format("Data Source={0};Initial Catalog={1};User ID={2};password={3}", db_Address.Text, db_BaseName.Text, db_Name.Text, db_Pass.Text);
+        }
+
+        //监听数据
+        private void Listen()
+        {
+
+            //程序的listener一直不关闭
+            //listener.Close();
+        }
+
+        //线程内向文本框txtRecvMssg中添加字符串及委托
+        private delegate void PrintRecvMssgDelegate(string s);
+        private void PrintRecvMssg(string info)
+        {
+            LogEx.l(info);
+            string sql = string.Format("INSERT INTO {0} ({1},{2}) VALUES('{3}','{4}')", TableName, ColumnInfo, ColumnTime, info, DateTime.Now);
+            var i=  DbHelperSQL.ExecuteSql(sql);
+            LogEx.l(i > 0 ? "Insert Success" : "Insert Fail");
+            //txtRecvMssg.Text += string.Format("[{0}]:{1}\r\n",
+            //    DateTime.Now.ToLongTimeString(), info);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(db_Address.Text) || string.IsNullOrWhiteSpace(db_BaseName.Text) || string.IsNullOrWhiteSpace(TableName) || string.IsNullOrWhiteSpace(db_Name.Text) || string.IsNullOrWhiteSpace(db_Pass.Text) || string.IsNullOrWhiteSpace(ColumnInfo) || string.IsNullOrWhiteSpace(ColumnTime))
+            {
+                MessageBox.Show("请先完善数据库信息并保存！");
+                return;
+            }
+            this.button1.Enabled = false;
+            if (comboBox1.SelectedIndex == 0)
+            {
+                Thread thrListener = new Thread(new ThreadStart(UDPListen));
+                thrListener.Start();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                Thread thrListener = new Thread(new ThreadStart(TCPClientListen));
+                thrListener.Start();
+            }
+            else
+            {
+                Thread thrListener = new Thread(new ThreadStart(TCPServerListen));
+                thrListener.Start();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //强制关闭程序（强行终止Listener）
+            Environment.Exit(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(IPAddress.Parse("127.0.0.1"), Convert.ToInt32(textBox1.Text));
+
+            NetworkStream ntwStream = tcpClient.GetStream();
+            if (ntwStream.CanWrite)
+            {
+                Byte[] bytSend = Encoding.UTF8.GetBytes(textBox2.Text);
+                ntwStream.Write(bytSend, 0, bytSend.Length);
+            }
+            else
+            {
+                MessageBox.Show("无法写入数据流");
+
+                ntwStream.Close();
+                tcpClient.Close();
+
+                return;
+            }
+
+            ntwStream.Close();
+            tcpClient.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(IPAddress.Parse("127.0.0.1"), Convert.ToInt32(textBox1.Text));
+
+            Byte[] bytSend = Encoding.UTF8.GetBytes(textBox2.Text);
+            udpClient.Send(bytSend, bytSend.Length);
+
+            udpClient.Close();
+        }
+
+
+        public void TCPClientListen()
+        {
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(new IPEndPoint(IPAddress.Any, Convert.ToInt32(textBox1.Text)));
+
+            //不断监听端口
+            while (true)
+            {
+                listener.Listen(10);
+                Socket socket = listener.Accept();
+                NetworkStream ntwStream = new NetworkStream(socket);
+                StreamReader strmReader = new StreamReader(ntwStream);
+                Invoke(new PrintRecvMssgDelegate(PrintRecvMssg),
+                    new object[] { strmReader.ReadToEnd() });
+                socket.Close();
+            }
+        }
+
+        public void TCPServerListen()
+        {
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(new IPEndPoint(IPAddress.Any, Convert.ToInt32(textBox1.Text)));
+
+            //不断监听端口
+            while (true)
+            {
+                listener.Listen(10);
+                Socket socket = listener.Accept();
+
+                byte[] buffer = new byte[18];
+                int receiveNumber = socket.Receive(buffer);
+                //  string message = Encoding.Default.GetString(buffer, 0, receiveNumber);
+
+                string message = byteToHexStr(buffer); //强制转换成16进制字符串
+                Invoke(new PrintRecvMssgDelegate(PrintRecvMssg),
+
+                    new object[] { message });
+                socket.Close();
+            }
+        }
+
+        public void UDPListen()
+        {
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Dgram, ProtocolType.Udp);
+            byte[] buffer = new byte[1024];
+            EndPoint point = new IPEndPoint(IPAddress.Any, Convert.ToInt32(textBox1.Text));
+            listener.Bind(point);
+
+            //不断监听端口
+            while (true)
+            {
+                //Socket socket = listener.Accept();
+                int Length = listener.ReceiveFrom(buffer, ref point);
+                string message = Encoding.Default.GetString(buffer, 0, Length);
+                Invoke(new PrintRecvMssgDelegate(PrintRecvMssg),
+                    new object[] { message });
+            }
+        }
+
+        /// <summary>
+        /// 字节数组转16进制字符串
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static string byteToHexStr(byte[] bytes)
+        {
+            string returnStr = "";
+            if (bytes != null)
+            {
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    returnStr += " " + bytes[i].ToString("X2");
+                }
+            }
+            return returnStr;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var strSec = Path.GetFileNameWithoutExtension(Ini.strFilePath);
+            Ini.Wini("db_Address", db_Address.Text.Trim());
+            Ini.Wini("db_Name", db_Name.Text.Trim());
+            Ini.Wini("db_Pass", db_Pass.Text.Trim());
+            Ini.Wini("db_BaseName", db_BaseName.Text.Trim());
+            Ini.Wini("db_TableName", db_TableName.Text.Trim());
+            Ini.Wini("db_ColumnInfo", db_ColumnInfo.Text.Trim());
+            Ini.Wini("db_ColumnTime", db_ColumnTime.Text.Trim());
+
+            TableName = db_TableName.Text;
+            ColumnInfo = db_ColumnInfo.Text;
+            ColumnTime = db_ColumnTime.Text;
+
+            DbHelperSQL.connectionString = string.Format("Data Source={0};Initial Catalog={1};User ID={2};password={3}", db_Address.Text, db_BaseName.Text, db_Name.Text, db_Pass.Text);
+            MessageBox.Show("保存成功");
+        }
+
+    }
+}
